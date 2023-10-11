ef4c0aeae7c1a93ec0b1bd6edf74e61d552252da
Samuel Abada <abadasamuelosp@gmail.com>
2023-10-11 12:56:41 +0100

    Update cli with base commands.

--- cli.dart
+++ cli.dart
@@ -5,7 +5,6 @@ void main(List<String> arguments) {
   final parser = ArgParser()
     ..addCommand('init')
     ..addCommand('apply')
-    ..addCommand('export-store-images')
     ..addCommand('update')
     ..addCommand('check');
 
@@ -14,20 +13,24 @@ void main(List<String> arguments) {
   switch (results.command?.name) {
     case 'init':
       // Create a sample flutter_config.yaml
+      initializeConfig();
       break;
     case 'apply':
       // Read flutter_config.yaml
-      // Apply metadata to AndroidManifest.xml and Info.plist
-      // Replace icons and splash screens
-      break;
-    case 'export-store-images':
-      // Export store images to a folder or zip
+      final config = loadConfig('path/to/flutter_config.yaml');
+      applyMetadata(config['metadata']);
+      applyPlatformSpecificConfig(config['platform_specific']);
+      applyVisualAssets(config['visual_assets']);
+      applyIntegrations(config['integrations']);
+      applySigningDetails(config['signing_details']);
       break;
     case 'update':
-      // Update platform-specific files
+      // Update platform-specific files based on flutter_config.yaml
+      updatePlatformFiles();
       break;
     case 'check':
       // Check if everything is up-to-date
+      checkUpdates();
       break;
     default:
       print('Unsupported command');
@@ -35,3 +38,38 @@ void main(List<String> arguments) {
   }
 }
 
+void initializeConfig() {
+  // Generate a sample flutter_config.yaml with default values or placeholders
+}
+
+Map<String, dynamic> loadConfig(String path) {
+  // Load the flutter_config.yaml and return its content as a Map
+}
+
+void applyMetadata(Map<String, dynamic> metadata) {
+  // Apply app name, display name, description, version, and build number
+}
+
+void applyPlatformSpecificConfig(Map<String, dynamic> platformConfig) {
+  // Apply Android and iOS specific configurations
+}
+
+void applyVisualAssets(Map<String, dynamic> visualAssets) {
+  // Apply icons, splash screens, and store images
+}
+
+void applyIntegrations(Map<String, dynamic> integrations) {
+  // Apply Firebase, analytics tools, and other integrations
+}
+
+void applySigningDetails(Map<String, dynamic> signingDetails) {
+  // Apply Android and iOS signing details
+}
+
+void updatePlatformFiles() {
+  // Update platform-specific files based on the latest Flutter version or other criteria
+}
+
+void checkUpdates() {
+  // Check if the platform-specific files are up-to-date with the flutter_config.yaml
+}
