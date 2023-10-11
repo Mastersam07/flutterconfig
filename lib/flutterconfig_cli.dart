432446ca1cedab10fe0224d1449771b8ce9abdcf
Mastersam07 <abadasamuelosp@gmail.com>
2023-10-12 00:22:24 +0100

    :rocket: cli scaffold

---
+++ lib/flutterconfig_cli.dart
@@ -0,0 +1,10 @@
+/// flutterconfig_cli, FlutterConfig CLI to help with configurations in your applications.
+///
+/// ```sh
+/// # activate flutterconfig_cli
+/// dart pub global activate flutterconfig_cli
+///
+/// # see usage
+/// flutterconfig --help
+/// ```
+library flutterconfig_cli;
